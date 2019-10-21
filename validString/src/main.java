f2cdb6111be333afcac846482a85a5e9e3df5b14
Claranc <fxj_bupt@163.com>
2019-10-21 14:27:55 +0800

    fix

--- validString/src/main.java
+++ validString/src/main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /*
  * 验证四则运算表达式的合法性以及计算结果
- * 输入: 四则运算表达式 e.g: (3-2)*5-1
+ * 输入: 四则运算表达式 e.g: (3-2)*5-1 (可带有一个位置变量x)
  * 输出: 合法表达式-计算结果/非法表达式-抛出异常
  */
 class main {
@@ -16,7 +16,6 @@ class main {
                 str2.append(str.charAt(i));
             }
         }
-//        System.out.println("Result: "+ recursive.biaodashi(str2.toString()));
         System.out.println("Result: "+ CalculatorTree.calculate(str2.toString()));
     }
 }
