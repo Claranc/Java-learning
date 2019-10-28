d0659d466c4977344e36a30d4232798b15fba8f7
fengxinjie <fengxinjie@bytedance.com>
2019-10-28 23:35:09 +0800

    fix

--- thread/src/Main.java
+++ thread/src/Main.java
@@ -3,12 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         String path = "/Users/fengxinjie/go/src/";
-        FileAnalysis fa = new FileAnalysis();
-        try {
-            fa.TraversingDictory("fxj", path);
-        } catch(IOException e) {
-            e.getStackTrace();
-        }
-
+        Thread t = new Thread(new ThreadAdded(path, "fxj"));
+        t.start();
     }
 }
--- thread/src/ThreadAdded.java
+++ thread/src/ThreadAdded.java
@@ -1,35 +1,59 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadAdded implements Runnable {
-    private Thread t;
-    private String threadName;
-    private FileAnalysis fa;
+    private static AtomicInteger threadCount = new AtomicInteger();
     private String path;
     private String target;
 
-    public ThreadAdded(String name, FileAnalysis f, String p, String t) {
-        threadName = name;
-        fa = f;
+    public ThreadAdded(String p, String t) {
         path = p;
         target = t;
     }
 
     @Override
     public void run() {
-        //System.out.println(threadName + "RUN...");
-        try {
-            fa.TraversingDictory(target, path);
-        } catch (IOException e) {
-            e.printStackTrace();
+        int curCnt = threadCount.incrementAndGet();
+        System.out.println(String.format("Start: %d running", curCnt));
+
+        File file = new File(path);
+        File[] lists = file.listFiles();
+        assert lists != null;
+        for (File list : lists) {
+            if (list.isFile()) {
+                //System.out.println("文件：" + lists[i]);
+                try {
+                    SearchFromFile(target, list.getPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (list.isDirectory()) {
+                //System.out.println("文件夹：" + lists[i]);
+                Thread newThread = new Thread(new ThreadAdded(list.getPath(), target));
+                newThread.start();
+            }
         }
 
+        curCnt = threadCount.decrementAndGet();
+        System.out.println(String.format("Exit: %d running", curCnt));
     }
 
-    public void start() {
-        //System.out.println(threadName + "Start...");
-        if (t == null) {
-            t = new Thread (this, threadName);
-            t.start();
+    public void SearchFromFile(String target, String filePath) throws IOException {
+        File file = new File(filePath);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line = "";
+        int num = 0;
+        while((line = br.readLine()) != null) {
+            num++;
+            if(line.contains(target)) {
+                System.out.println(filePath + " " + num + " " + line);
+            }
         }
+        fr.close();
+        br.close();
     }
 }
